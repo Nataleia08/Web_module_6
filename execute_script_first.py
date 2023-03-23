8cd96b12df8f472afaecbf5385d75f8224d87198
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-23 23:15:16 +0200

    draft 4

--- execute_script_first.py
+++ execute_script_first.py
@@ -3,17 +3,17 @@ from psycopg2 import connect, Error
 from contextlib import contextmanager
 
 @contextmanager
-def new_connect(conn_host, conn_user, conn_password, conn_port):
+def new_connect():
     conn = None
     try:
-        conn = connect(host = conn_host, user = conn_user, password = conn_password, port = conn_port)
+        conn = connect(host = 'localhost', user = 'postgres', password = '112233445566', port = 5432)
         yield conn
     except Error as err:
         print("Error:", err)
-        conn.rollback()
+        # conn.rollback()
     finally:
         if conn:
-            conn.close
+            conn.close()
 
 
 def execute_script(conn, sql_script):
@@ -22,17 +22,26 @@ def execute_script(conn, sql_script):
     c.close()
 
 
-if __name__ == '__main__':
-    sql_1 = """CREATE TABLE people(
-  id INT PRIMARY KEY,
-  full_name VARCHAR(150),
-  day_birthday DATE, 
-  email VARCHAR(30),
-  phone VARCHAR(15) UNIQUE NOT NULL, 
-  gender_id INT, 
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-  last_update_at TIMESTAMP
-);"""
 
-with new_connect("localhost", 'postgres', '112233445566', 5432) as conn:
-    execute_script(conn, sql_1)
+# def create_table():
+#     for i in range(7):
+#         with open(f"querty_{i+1}.sql", "r") as f:
+#             sql = f.read()
+#         with new_connect() as conn:
+#             execute_script(conn, sql)
+
+
+# if __name__ == '__main__':
+#   sql_1 = """CREATE TABLE people(
+#   id INT PRIMARY KEY,
+#   full_name VARCHAR(150),
+#   day_birthday DATE, 
+#   email VARCHAR(30),
+#   phone VARCHAR(15) UNIQUE NOT NULL, 
+#   gender_id INT, 
+#   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+#   last_update_at TIMESTAMP
+# );"""
+
+# with new_connect() as conn:
+#     execute_script(conn, sql_1)
--- insert_info.py
+++ insert_info.py
@@ -1,33 +1,26 @@
 from faker import Faker
 from random import randint, choice 
-# from execute_script_first import new_connect
+from execute_script_first import new_connect
 from datetime import datetime, timedelta
 from psycopg2 import connect
 import random
-
 from contextlib import contextmanager
 
-@contextmanager
-def new_connect(conn_host, conn_user, conn_password, conn_port):
-    conn = None
-    try:
-        conn = connect(host = conn_host, user = conn_user, password = conn_password, port = conn_port)
-        yield conn
-    except OSError as err:
-        print("Error:", err)
-        conn.rollback()
-    finally:
-        if conn:
-            conn.close
 
 
 fake = Faker("uk-UA")
+# Faker.seed(0)
 
 if __name__ == "__main__":
     sql_1 = """INSERT INTO people (id, full_name, age, email, phone) VALUES (%s, %s, %s, %s, %s)"""
 
-    with new_connect('localhost', 'postgres', '112233445566', 5432) as conn:
+    with new_connect() as conn:
         c = conn.cursor()
-        for i in range(100):
-            c.execute(sql_1, (i+1, fake.name_male(), randint(18, 90), fake.email(), fake.phone_number()))
+        for _ in range(100):
+            print(fake.iana_id(), fake.name_male(), randint(18, 90), fake.email(), fake.phone_number())
+            c.execute(sql_1, (fake.iana_id(), fake.name_male(), randint(18, 90), fake.email(), fake.phone_number()))
+            conn.commit()
+            # print(c.fetchone())
         c.close()
+
+    sql_2 = """INSERT INTO subject"""
