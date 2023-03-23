8e07ebead4cd48e3418b4ff9803de2e34bb88316
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-23 23:32:38 +0200

    draft 5

--- insert_info.py
+++ insert_info.py
@@ -11,16 +11,26 @@ from contextlib import contextmanager
 fake = Faker("uk-UA")
 # Faker.seed(0)
 
+GROUP_LIST = ["КА-56", "МА-98", "СА-33"]
+
 if __name__ == "__main__":
     sql_1 = """INSERT INTO people (id, full_name, age, email, phone) VALUES (%s, %s, %s, %s, %s)"""
 
     with new_connect() as conn:
         c = conn.cursor()
-        for _ in range(100):
-            print(fake.iana_id(), fake.name_male(), randint(18, 90), fake.email(), fake.phone_number())
-            c.execute(sql_1, (fake.iana_id(), fake.name_male(), randint(18, 90), fake.email(), fake.phone_number()))
+        for i in range(200):
+            print(i+1, fake.name_male(), randint(18, 90), fake.email(), fake.phone_number())
+            c.execute(sql_1, (i+1, fake.name_male(), randint(18, 90), fake.email(), fake.phone_number()))
             conn.commit()
             # print(c.fetchone())
         c.close()
 
-    sql_2 = """INSERT INTO subject"""
+    sql_2 = """INSERT INTO stu_groups (id, group_number, kurs, daytime) VALUES (%s, %s, %s, %s)"""
+
+    with new_connect() as conn:
+        c = conn.cursor()
+        for i, j in GROUP_LIST, range(3):
+            c.execute(sql_1, (j, i, randint(1, 5), randint(1, 2)))
+            conn.commit()
+            print(c.fetchone())
+        c.close()
