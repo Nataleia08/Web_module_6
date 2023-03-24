4f2739a5b9ed1854d5859ebe2904a641a6b6ee28
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-25 00:10:36 +0200

    version 1.1

--- insert_info.py
+++ insert_info.py
@@ -51,26 +51,26 @@ def random_date(date_start: datetime, date_end: datetime):
 
 if __name__ == "__main__":
 
-    # sql_2 = """INSERT INTO stu_groups (id, group_number, kurs, daytime) VALUES (%s, %s, %s, %s)"""
+    sql_2 = """INSERT INTO stu_groups (id, group_number, kurs, daytime) VALUES (%s, %s, %s, %s)"""
 
-    # with new_connect() as conn:
-    #     c = conn.cursor()
-    #     j = 1
-    #     for i in GROUP_LIST:
-    #         c.execute(sql_2, (j, i, randint(1, 5), randint(1, 2)))
-    #         conn.commit()
-    #         j +=1 
-    #     c.close()
+    with new_connect() as conn:
+        c = conn.cursor()
+        j = 1
+        for i in GROUP_LIST:
+            c.execute(sql_2, (j, i, randint(1, 5), randint(1, 2)))
+            conn.commit()
+            j +=1 
+        c.close()
 
 
-    # sql_3 = """INSERT INTO students (id, full_name, age, email, phone, budget, scholarship, group_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+    sql_3 = """INSERT INTO students (id, full_name, age, email, phone, budget, scholarship, group_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
 
-    # with new_connect() as conn:
-    #     c = conn.cursor()
-    #     for i in range(50):
-    #         c.execute(sql_3, (i+1, fake.name(), randint(18, 30), fake.email(), fake.phone_number(), bool(randint(0, 1)), bool(randint(0, 1)), choice(range(1, 3))))
-    #         conn.commit()
-    #     c.close()
+    with new_connect() as conn:
+        c = conn.cursor()
+        for i in range(50):
+            c.execute(sql_3, (i+1, fake.name(), randint(18, 30), fake.email(), fake.phone_number(), bool(randint(0, 1)), bool(randint(0, 1)), choice(range(1, 3))))
+            conn.commit()
+        c.close()
 
 
     sql_4 = """INSERT INTO teachers (id, full_name, age, email, phone, date_start_work, scientific_degree, salary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     with new_connect() as conn:
         c = conn.cursor()
         for i in range(6):
-            c.execute(sql_4, (i, fake.name(), randint(35, 90), fake.email(), fake.phone_number(), random_date(datetime(year=2013, month=1, day=1), datetime(year=2023, month=1, day=1)), choice(scientific_degree_list),  randint(20000, 50000)))
+            c.execute(sql_4, (i+1, fake.name(), randint(35, 90), fake.company_email(), fake.phone_number(), random_date(datetime(year=2013, month=1, day=1), datetime(year=2023, month=1, day=1)), choice(scientific_degree_list),  randint(20000, 50000)))
             conn.commit()
         c.close()
 
@@ -88,19 +88,19 @@ if __name__ == "__main__":
         c = conn.cursor()
         j = 1
         for i in SUBJECT_LIST:
-            c.execute(sql_5, (j, i, choice(range(6))))
+            c.execute(sql_5, (j, i, choice(range(1, 5))))
             conn.commit()
             j += 1
         c.close()     
 
-    sql_6 = """INSERT INTO grades (id, date_create, grade, number_grade, student_id, subject_id) VALUES (%s, %s, %s)"""
+    sql_6 = """INSERT INTO grades (id, date_create, grade, number_grade, student_id, subject_id) VALUES (%s, %s, %s, %s, %s, %s)"""
 
     with new_connect() as conn:
         c = conn.cursor()
         j = 1
         for s in range(50):
             for g in range(20):
-                c.execute(sql_6, (j, random_date(datetime(year=2022, month=9, day=1), datetime(year=2023, month=3, day=24)), choice(GRADE_LIST), randint(30, 100), s, randint(1, 7)))
+                c.execute(sql_6, (j, random_date(datetime(year=2022, month=9, day=1), datetime(year=2023, month=3, day=24)), choice(GRADE_LIST), randint(30, 100), s+1, randint(1, 7)))
                 conn.commit()
                 j += 1
         c.close()
