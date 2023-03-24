cbef25f07331739451dacd734ad161be2784ee0c
Nataleia Orlovska <nataleia.orlovska@gmail.com>
2023-03-25 01:05:14 +0200

    version 1.2

--- execute_script_first.py
+++ execute_script_first.py
@@ -18,14 +18,13 @@ def new_connect():
 
 
 def create_table():
-    for i in range(1, 7):
+    for i in range(2, 7):
         with open(f"querty_{i+1}.sql", "r") as f:
             sql = f.read()
         with new_connect() as conn:
             c = conn.cursor()
             c.execute_script(conn, sql)
             c.close()
-
 
 
 if __name__ == '__main__':
--- insert_info.py
+++ insert_info.py
@@ -51,47 +51,47 @@ def random_date(date_start: datetime, date_end: datetime):
 
 if __name__ == "__main__":
 
-    sql_2 = """INSERT INTO stu_groups (id, group_number, kurs, daytime) VALUES (%s, %s, %s, %s)"""
+    # sql_2 = """INSERT INTO stu_groups (id, group_number, kurs, daytime) VALUES (%s, %s, %s, %s)"""
 
-    with new_connect() as conn:
-        c = conn.cursor()
-        j = 1
-        for i in GROUP_LIST:
-            c.execute(sql_2, (j, i, randint(1, 5), randint(1, 2)))
-            conn.commit()
-            j +=1 
-        c.close()
-
-
-    sql_3 = """INSERT INTO students (id, full_name, age, email, phone, budget, scholarship, group_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
-
-    with new_connect() as conn:
-        c = conn.cursor()
-        for i in range(50):
-            c.execute(sql_3, (i+1, fake.name(), randint(18, 30), fake.email(), fake.phone_number(), bool(randint(0, 1)), bool(randint(0, 1)), choice(range(1, 3))))
-            conn.commit()
-        c.close()
+    # with new_connect() as conn:
+    #     c = conn.cursor()
+    #     j = 1
+    #     for i in GROUP_LIST:
+    #         c.execute(sql_2, (j, i, randint(1, 5), randint(1, 2)))
+    #         conn.commit()
+    #         j +=1 
+    #     c.close()
 
 
-    sql_4 = """INSERT INTO teachers (id, full_name, age, email, phone, date_start_work, scientific_degree, salary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+    # sql_3 = """INSERT INTO students (id, full_name, age, email, phone, budget, scholarship, group_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
 
-    with new_connect() as conn:
-        c = conn.cursor()
-        for i in range(6):
-            c.execute(sql_4, (i+1, fake.name(), randint(35, 90), fake.company_email(), fake.phone_number(), random_date(datetime(year=2013, month=1, day=1), datetime(year=2023, month=1, day=1)), choice(scientific_degree_list),  randint(20000, 50000)))
-            conn.commit()
-        c.close()
+    # with new_connect() as conn:
+    #     c = conn.cursor()
+    #     for i in range(50):
+    #         c.execute(sql_3, (i+1, fake.name(), randint(18, 30), fake.email(), fake.phone_number(), bool(randint(0, 1)), bool(randint(0, 1)), choice(range(1, 4))))
+    #         conn.commit()
+    #     c.close()
 
-    sql_5 = """INSERT INTO subjects (id, title, teacher_id) VALUES (%s, %s, %s)"""
 
-    with new_connect() as conn:
-        c = conn.cursor()
-        j = 1
-        for i in SUBJECT_LIST:
-            c.execute(sql_5, (j, i, choice(range(1, 5))))
-            conn.commit()
-            j += 1
-        c.close()     
+    # sql_4 = """INSERT INTO teachers (id, full_name, age, email, phone, date_start_work, scientific_degree, salary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+
+    # with new_connect() as conn:
+    #     c = conn.cursor()
+    #     for i in range(6):
+    #         c.execute(sql_4, (i+1, fake.name(), randint(35, 90), fake.company_email(), fake.phone_number(), random_date(datetime(year=2013, month=1, day=1), datetime(year=2023, month=1, day=1)), choice(scientific_degree_list),  randint(20000, 50000)))
+    #         conn.commit()
+    #     c.close()
+
+    # sql_5 = """INSERT INTO subjects (id, title, teacher_id) VALUES (%s, %s, %s)"""
+
+    # with new_connect() as conn:
+    #     c = conn.cursor()
+    #     j = 1
+    #     for i in SUBJECT_LIST:
+    #         c.execute(sql_5, (j, i, choice(range(1, 5))))
+    #         conn.commit()
+    #         j += 1
+    #     c.close()     
 
     sql_6 = """INSERT INTO grades (id, date_create, grade, number_grade, student_id, subject_id) VALUES (%s, %s, %s, %s, %s, %s)"""
 
